10326fb9dd45be7f86fb639d187c27e095ac1c1c
reiulen <reiandika10@gmail.com>
2021-11-24 18:23:39 +0700

    e perpus

--- resources/views/backend/buku/kategori.blade.php
+++ resources/views/backend/buku/kategori.blade.php
@@ -90,9 +90,8 @@
 
 
       <!-- Modal -->
-      @foreach($kategori as $row)
-        <div class="modal fade" id="modalTambah" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal fade" id="modalTambah" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered" role="document">
                 <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLongTitle">Tambah Kategori</h5>
@@ -119,7 +118,7 @@
                 </div>
             </div>
         </div>
-
+      @foreach($kategori as $row)
         <div class="modal fade" id="modalEdit{{ $row->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered" role="document">
                 <div class="modal-content">
